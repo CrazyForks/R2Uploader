ee44a755b439ae086dcd9520ca6057a03bcf73a1
jlvihv <imvihv@gmail.com>
2024-12-31 19:56:59 +0800

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+import "../app.css";
+import { onNavigate } from "$app/navigation";
+let { children } = $props();
+
+onNavigate((navigation) => {
+	if (!document.startViewTransition) return;
+
+	return new Promise((resolve) => {
+		document.startViewTransition(async () => {
+			resolve();
+			await navigation.complete;
+		});
+	});
+});
+</script>
+
+
+<div class="min-h-svh bg-slate-50 dark:bg-slate-900">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+import { Dashboard } from "@uppy/svelte";
+import Uppy from "@uppy/core";
+
+// Don't forget the CSS: core and UI components + plugins you are using
+import "@uppy/core/dist/style.css";
+import "@uppy/dashboard/dist/style.css";
+import "@uppy/file-input/dist/style.css";
+import FileInput from "@uppy/file-input";
+
+const uppy = new Uppy().use(FileInput);
+</script>
+
+<main><Dashboard uppy={uppy} /></main>
